e1f9d5a933b3836503c05a29cdb7c153e382f0d7
7hsiamv1 <7hsiam123@gmail.com>
2026-06-26 08:29:47 +0600

    Update title in HTML header

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ HOME_HTML = """<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title>Premium FF Nickname Checker</title>
+<title>FF Nickname Checker By 7H SIAM</title>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
 <style>
   :root {
